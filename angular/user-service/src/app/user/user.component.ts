4edc43a5a5693efd2e8f46470937ae00c06fa0ee
techmomo <mohsin.khursheed@techjini.com>
2020-12-17 13:09:56 +0530

    get current dropdown select

--- angular/user-service/src/app/user/user.component.ts
+++ angular/user-service/src/app/user/user.component.ts
@@ -58,4 +58,38 @@ export class UserComponent implements OnInit {
       this.errorMessage = 'No Users Found...!'
     }
   }
+  eGetId(event){
+    console.log(event.value);
+  }
 }
+
+// ng new user-service // create a new angular project
+// ng g c user // create a new component
+// ng g c footer 
+// ng g c header
+
+// object structure
+// let employees = [
+//   { // eid ename project name pbudget 
+//     eid: 1,
+//     ename: 'XYZ',
+//     email: 'a@gg.com',
+//     project:{
+//       pid: 1,
+//       pname: 'Roche',
+//       budget: 10000
+//     },
+//     salary: 500
+//   },
+//   {
+//     eid: 2,
+//     ename: 'XYZ',
+//     email: 'a@gg.com',
+//     project:{
+//       pid:2,
+//       pname: 'ATT',
+//       budget: 7000
+//     },
+//     salary: 600
+//   }
+// ]
