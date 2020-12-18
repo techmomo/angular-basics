e69353af49710579df75bfeaff53527236ad7d8d
techmomo <mohsin.khursheed@techjini.com>
2020-12-18 10:47:30 +0530

    services

---
+++ angular/user-service/src/app/services/user.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor() { }
+
+  getMessage():void{
+    console.log("User Service : getMessage()");
+  }
+  addDays = (days:number):Date=>{
+    let today:Date = new Date();
+    today.setDate(today.getDate() + days);
+    return today;
+  }
+  buildUsers():any[]{
+    return [
+      {
+        id: 1,
+        name: 'User1',
+        email: 'user1@gg.com',
+        dob: this.addDays(1)
+      },
+      {
+        id: 2,
+        name: 'User2',
+        email: 'user2@gg.com',
+        dob: this.addDays(2)
+      },
+      {
+        id: 3,
+        name: 'User3',
+        email: 'user3@gg.com',
+        dob: this.addDays(3)
+      },
+      {
+        id: 4,
+        name: 'User4',
+        email: 'user4@gg.com',
+        dob: this.addDays(4)
+      }
+    ]
+  }
+  deleteUserById(users:any[],id:string):any{
+    const index = users.findIndex(u=>u.id == id);
+    let obj = {
+      users: users,
+      errorMessage : ''
+    }
+    // delete found user
+    users.splice(index,1); // using spice based on index 
+    if(users.length == 0){
+      obj.users = undefined;
+      obj.errorMessage = 'No Users Found...!'
+    }
+    return obj;
+  }
+  getUserById(users:any[],id:string){
+
+  }
+}
--- angular/user-service/src/app/user/user.component.ts
+++ angular/user-service/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,64 +10,21 @@ export class UserComponent implements OnInit {
 
   users:any[];
   errorMessage:string;
-  constructor() { }
+  // inject userservice into user component
+  constructor(private service:UserService) { }
 
   ngOnInit(): void {
-    this.addUsers(); // build user array
+    this.users = this.service.buildUsers(); // build user array  
   }
-  addUsers():void{
-    this.users=[
-      {
-        id: 1,
-        name: 'User1',
-        email: 'user1@gg.com',
-        dob: this.addDays(1)
-      },
-      {
-        id: 2,
-        name: 'User2',
-        email: 'user2@gg.com',
-        dob: this.addDays(2)
-      },
-      {
-        id: 3,
-        name: 'User3',
-        email: 'user3@gg.com',
-        dob: this.addDays(3)
-      },
-      {
-        id: 4,
-        name: 'User4',
-        email: 'user4@gg.com',
-        dob: this.addDays(4)
-      }
-    ]
-  }
-  addDays = (days:number):Date=>{
-    let today:Date = new Date();
-    today.setDate(today.getDate() + days);
-    return today;
-  }
-
   // html events
   eDeleteUser(id:string){
-    const index = this.users.findIndex(u=>u.id == id);
-    // delete found user
-    this.users.splice(index,1); // using spice based on index 
-    if(this.users.length == 0){
-      this.users = undefined;
-      this.errorMessage = 'No Users Found...!'
-    }
+    let obj = this.service.deleteUserById(this.users,id);
+    this.users = obj.users;
+    this.errorMessage = obj.errorMessage;
+
   }
   eGetId(event){
     console.log(event.value);
-    this.filterBySalary(1);
-  }
-  filterBySalary(pid:number):void{
-    this.users.sort((a,b)=>{ 
-      return b.id - a.id;
-    });
-    console.log(this.users);
   }
 }
 
